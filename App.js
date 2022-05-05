89a4547002a4c3d672c334115ee1a54832ab63a9
JosueGiithub <100323728+JosueGiithub@users.noreply.github.com>
2022-05-05 20:28:40 -0300

    PROVA
    PROVA

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { useCallback, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { Button, ButtonGroup, Input, Text } from 'react-native-elements';
+import  Icon  from 'react-native-vector-icons/FontAwesome';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from './screens/Login';
+import Principal from './screens/Principal';
+import 'react-native-gesture-handler';
+import styles from './Style/MainStyle';
+
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Principal" component={Principal} />
+    </Stack.Navigator>
+  );
+}
+export default function App() {
+  return(
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
+  )
+   
+}
