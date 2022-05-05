6b3e5913d09c6d58d17ba0f4cdf3c10eae9de000
JosueGiithub <100323728+JosueGiithub@users.noreply.github.com>
2022-05-05 20:46:50 -0300

    Update App.js

--- App.js
+++ App.js
@@ -3,12 +3,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { Button, ButtonGroup, Input, Text } from 'react-native-elements';
 import  Icon  from 'react-native-vector-icons/FontAwesome';
+import Styles from './style/MainStyle';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Login from './screens/Login';
 import Principal from './screens/Principal';
 import 'react-native-gesture-handler';
-import styles from './Style/MainStyle';
 
 
 const Stack = createStackNavigator();
